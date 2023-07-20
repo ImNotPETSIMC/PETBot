3d72b8d1c89124f409aad18b2d13d65c191a6bee
Enzo Weder <enzowederma@gmail.com>
2023-07-20 13:48:35 -0300

    ✨ feat: new Member class

--- src/classes.ts
+++ src/classes.ts
@@ -9,3 +9,27 @@ export class Embed {
         this.color = parseInt(color, 16);
     };
 }
+
+export class Member {
+    name: string;
+    photo_url: string;
+    register_code: string;
+    admission_year: number;
+    email: string;
+    instagram_url: string;
+    linkedin_url: string;
+    lattes_url: string;
+    status: string;
+
+    constructor(name: string, photo_url: string, register_code: string, admission_year: number, email: string, instagram_url: string, linkedin_url: string, lattes_url: string, status: string) {
+        this.name = name;
+        this.photo_url = photo_url;
+        this.register_code = register_code;
+        this.admission_year = admission_year;
+        this.email = email;
+        this.instagram_url = instagram_url;
+        this.linkedin_url = linkedin_url;
+        this.lattes_url = lattes_url;
+        this.status = status;
+    };
+}
