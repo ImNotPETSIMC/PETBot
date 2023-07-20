9ad899bf8f578422fb9a8a997f1ddcc96e38eeb7
Enzo Weder <enzowederma@gmail.com>
2023-07-20 13:20:41 -0300

    ✨ feat: add normalizeString helper

---
+++ src/helper/normalizeString.ts
@@ -0,0 +1,14 @@
+export const normalizeString = (string: string, type: string) => {
+    string = String(string);
+    if(string.replace(/\s+/g, ' ').trim().length <= 1) return string;
+    const dontCapitalize = ["de", "da", "do"];
+  
+    if(type != "register_code") {
+      return String(string).replace(/\s+/g, ' ').trim().split(" ").map(word => { 
+        if(dontCapitalize.includes(word)) return word;
+        return word[0].toUpperCase() + word.slice(1);
+      }).join(" ").trim();
+    }
+    return string.toUpperCase().trim();
+};
+  
